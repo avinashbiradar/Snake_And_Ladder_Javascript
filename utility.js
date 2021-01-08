ebe270dbe6236d5df257027a7725c8b51022b081
avinashbiradar <avinashbiradar179@gmail.com>
2021-01-08 19:50:23 +0530

    Added:Dice roll to get the dice numbers from 1 to 6

--- utility.js
+++ utility.js
@@ -2,6 +2,8 @@ let STARTING_POSITION = 0;
 class Utility{
      SnakeAndLadder=()=> {
           console.log(STARTING_POSITION);
+          var Random = Math.floor((Math.random()*6)+1);
+          console.log("Die Number is :"+Random);
         }
 }
 module.exports = new Utility();
